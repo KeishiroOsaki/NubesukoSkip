1d1dc0eff697773c58314600cfee695f42d69458
oosakikeishiro <startipend2008@gmail.com>
2015-06-19 18:24:07 +0900

    Viewを作るところまでは進展。 どうしても画面が表示されない。

--- app/src/main/java/com/example/oosakikeishiro/nubesukoskip/GameSFV.java
+++ app/src/main/java/com/example/oosakikeishiro/nubesukoskip/GameSFV.java
@@ -7,10 +7,16 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 /**
  * Created by oosakikeishiro on 15/06/19.
@@ -25,9 +31,16 @@ public class GameSFV extends SurfaceView implements Runnable, SurfaceHolder.Call
 
     private Bitmap star, arrowUp, arrowDown, hoge;
     private Bitmap[] player = new Bitmap[8];
-    private Boolean gameState;
+    private Boolean gameState = true;
+    private int turns = 0;
+    private int addscore = 0;
+
     private Thread mLooper;
 
+    boolean isAttached;
+
+
+    int blockWidth;
     private int map[][] = {
             {0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0},
@@ -41,12 +54,19 @@ public class GameSFV extends SurfaceView implements Runnable, SurfaceHolder.Call
     private long ival = 300;
     private int muteki;
 
+    private Rect src[], dst[][];
+    private Bitmap[] obImg;
+
+    posiUpTask timerTask = null;
+    Timer mTimer = null;
+    Handler mHandler = new Handler();
+
 
     public GameSFV(Context context, AttributeSet attrs) {
         super(context, attrs);
         setFocusable(true);
         surfaceHolder = getHolder();
-        surfaceHolder.addCallback(this);
+        getHolder().addCallback(this);
 
         //ここから拡張実装
         //まずは画像読み込み
@@ -60,9 +80,18 @@ public class GameSFV extends SurfaceView implements Runnable, SurfaceHolder.Call
         player[5] = BitmapFactory.decodeResource(res, R.drawable.player05);
         player[7] = BitmapFactory.decodeResource(res, R.drawable.player07);
         player[6] = BitmapFactory.decodeResource(res, R.drawable.player06);
+        obImg = new Bitmap[]{null, hoge, arrowUp, star, arrowDown};
 
-        Log.d("Notice","読み込み完了");
+        Log.d("Notice", "読み込み完了");
         //
+
+        src = new Rect[5];
+        src[1] = new Rect(0, 0, hoge.getWidth(), hoge.getHeight());
+        src[2] = new Rect(0, 0, arrowUp.getWidth(), arrowUp.getHeight());
+        src[3] = new Rect(0, 0, star.getWidth(), star.getHeight());
+        src[4] = new Rect(0, 0, arrowDown.getWidth(), arrowDown.getHeight());
+        dst = new Rect[6][5];
+
 
     }
 
@@ -70,16 +99,20 @@ public class GameSFV extends SurfaceView implements Runnable, SurfaceHolder.Call
     @Override
     public void run() {
 
-        while (mLooper != null) {
+        while (/*mLooper != null*/ /*isAttached*/ true) {
+
+            Log.d("チェックポイント", "while直下");
 
             //描画処理
-            doDraw();
+            doDraw(getHolder());
 
             //位置更新処理
 
+          /*
             //処理落ちによるスローモーションをさけるため現在時刻を取得
             long delta = System.currentTimeMillis() - mTime;
             Log.d("mtime", Long.toString(mTime));
+            Log.d("delta", Long.toString(delta));
             if (delta >= ival) {
 
                 mTime = System.currentTimeMillis();
@@ -90,9 +123,11 @@ public class GameSFV extends SurfaceView implements Runnable, SurfaceHolder.Call
                 //上のmapを下にコピー
                 for (int r = 5; r > 0; r--) {
                     for (int c = 0; c < 5; c++) {
-                        map[c][r] = map[c][r - 1];
+                        map[r][c] = map[r - 1][c];
                     }
                 }
+                Log.d("チェックポイント", "map転写完了");
+
 
                 //最上段に新規アサイン
                 map[0][(int) (Math.random() * 5)] = 1;
@@ -109,8 +144,12 @@ public class GameSFV extends SurfaceView implements Runnable, SurfaceHolder.Call
                     muteki--;
                 }
 
+                Log.d("チェックポイント", "上段障害物生成完了");
+
                 //当たり判定
                 if (muteki == 0) {
+
+
                     switch (map[uPosiY][uPosiX]) {
                         case 1: //障害物と衝突
                             die();
@@ -129,7 +168,7 @@ public class GameSFV extends SurfaceView implements Runnable, SurfaceHolder.Call
                             break;
 
                         case 4: //下矢印と衝突
-                            if (uPosiY >= 4) {
+                            if (uPosiY <= 4) {
                                 uPosiY++;
                             } else {
                                 die();
@@ -140,36 +179,46 @@ public class GameSFV extends SurfaceView implements Runnable, SurfaceHolder.Call
 
                     }
                 }
+                Log.d("チェックポイント", "当たり判定終了");
+
+                //doDraw();
+            }*/
 
 
-            }
         }
 
 
     }
 
     private void die() {
-
+        Log.d("チェックポイント", "突然の死");
+        gameState = false;
     }
 
     //描画関数
-    private void doDraw() {
+    private void doDraw(SurfaceHolder holder) {
 
+        Log.d("チェックポイント", "doDraw先頭");
         //描画処理
-        Canvas cvs = surfaceHolder.lockCanvas();
+        Canvas cvs = holder.lockCanvas();
         Paint paint = new Paint();
-        /*
-        c.drawBitmap(grass, 0, 0, paint);
-        c.drawBitmap(goburin, 250, 150, paint);
-        */
+
         cvs.drawColor(Color.WHITE);
 
-        float blockWidth = screen_width / 5;
+        blockWidth = screen_width / 5;
+
+        for (int c = 0; c < 5; c++) {
+            for (int r = 0; r < 6; r++) {
+                dst[r][c] = new Rect(c * blockWidth, r * blockWidth, (c + 1) * blockWidth, (r + 1) * blockWidth);
+            }
+        }
+
         for (int r = 0; r < 6; r++) {
             for (int c = 0; c < 5; c++) {
-                switch (map[c][r]) {
+                /*
+                switch (map[r][c]) {
                     case 1:
-                        cvs.drawBitmap(hoge, c * blockWidth, r * blockWidth, paint);
+                        cvs.drawBitmap(hoge, src[], r * blockWidth, paint);
                         break;
                     case 2:
                         cvs.drawBitmap(arrowUp, c * blockWidth, r * blockWidth, paint);
@@ -181,35 +230,123 @@ public class GameSFV extends SurfaceView implements Runnable, SurfaceHolder.Call
                         cvs.drawBitmap(arrowDown, c * blockWidth, r * blockWidth, paint);
                         break;
                 }
+                */
+                if (map[r][c] != 0) {
+                    cvs.drawBitmap(obImg[map[r][c]], src[map[r][c]], dst[r][c], paint);
+                }
             }
         }
 
-        surfaceHolder.unlockCanvasAndPost(cvs);
+
+        Log.d("チェックポイント", "描画完了");
+        holder.unlockCanvasAndPost(cvs);
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         screen_width = width;
         screen_height = height;
-        //スレッド処理を開始
-        if (mLooper != null) {
-            screen_height = height;
-            mTime = System.currentTimeMillis();
-            mLooper.start();
-        }
+
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         surfaceHolder = holder;
+        timerTask = new posiUpTask();
+        mTimer = new Timer(true);
         mLooper = new Thread(this);
+        isAttached = true;
+
+        doDraw(holder);
+        mTimer.schedule(timerTask, 0, 300);
         mLooper.start();
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        mLooper = null;
+
+        mTimer.cancel();
+        //
+        isAttached = false;
+
+        //  while (mLooper.isAlive()) {
+        // }
+
+        //mLooper = null;
+
     }
 
+    class posiUpTask extends TimerTask {
 
+        @Override
+        public void run() {
+            //次の描画位置
+
+            //上のmapを下にコピー
+            for (int r = 5; r > 0; r--) {
+                for (int c = 0; c < 5; c++) {
+                    map[r][c] = map[r - 1][c];
+                }
+            }
+            Log.d("チェックポイント", "map転写完了");
+
+
+            //最上段に新規アサイン
+            map[0][(int) (Math.random() * 5)] = 1;
+            map[0][(int) (Math.random() * 5)] = 2;
+            if (Math.random() * 100 > 95) {
+                map[0][(int) (Math.random() * 5)] = 3;
+            }
+
+            if (Math.random() * 100 > 70) {
+                map[0][(int) (Math.random() * 5)] = 4;
+            }
+
+            if (muteki > 0) {
+                muteki--;
+            }
+
+            Log.d("チェックポイント", "上段障害物生成完了");
+
+            //当たり判定
+            if (muteki == 0) {
+
+
+                switch (map[uPosiY][uPosiX]) {
+                    case 1: //障害物と衝突
+                        die();
+                        break;
+
+                    case 2: //上矢印と衝突
+                        if (uPosiY <= 1) {
+                            uPosiY--;
+                        } else {
+                            die();
+                        }
+                        break;
+
+                    case 3: //星と衝突
+                        addscore++;
+                        break;
+
+                    case 4: //下矢印と衝突
+                        if (uPosiY <= 4) {
+                            uPosiY++;
+                        } else {
+                            die();
+                        }
+
+                        break;
+
+
+                }
+            }
+            Log.d("チェックポイント", "当たり判定終了");
+
+
+            //doDraw(surfaceHolder);
+        }
+    }
 }
+
+
